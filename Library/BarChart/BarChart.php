eb3d373f7309bbf7d59ea4da09a07925588f5826
martinsik <martin.sikora@symbio.cz>
2011-02-15 21:56:48 +0100

    added bar chart class

---
+++ Library/BarChart/BarChart.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bundle\GoogleChartBundle\Library\PieChart;
+
+use Bundle\GoogleChartBundle\Library\Chart\AbstractAxisChart;
+
+class BarChart extends AbstractAxisChart {
+    
+    
+    
+}
